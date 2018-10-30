64476842a5b38dcd52a202d855a1e86a14a51c94
yekossynbay <conprefor@gmail.com>
2018-10-30 16:00:53 +0600

    lesson 6 - done

---
+++ lesson6 -  selectors in components/src/app/car/car.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.app-car',
+  template: `<h3>Somecar</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dignissimos natus optio vitae voluptates. Mollitia?</p>`,
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ lesson6 -  selectors in components/src/app/cars/cars.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: '[app-cars]', // атрибут у дива в app.html
+    templateUrl: './cars.component.html',
+    styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
