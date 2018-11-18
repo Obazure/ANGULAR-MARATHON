89d0ff588c0d3b09d391d17491a5200a96746a92
yekossynbay <conprefor@gmail.com>
2018-11-18 18:06:18 +0600

    21 lesson done

--- src/app/block4/directives/directive-first.directive.ts
+++ src/app/block4/directives/directive-first.directive.ts
@@ -1,14 +1,17 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
-  selector: '[appDirectiveFirst]'
+    selector: '[appDirectiveFirst]'
 })
-export class DirectiveFirstDirective implements  OnInit{
+export class DirectiveFirstDirective implements OnInit {
 
-  constructor(private element: ElementRef) { }
+    constructor(private element: ElementRef, private renderer: Renderer2) {
+    }
 
-  ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = 'blue'
-  }
+    ngOnInit() {
+        const {nativeElement} = this.element;
+        this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
+        this.renderer.addClass(nativeElement, 'white-text');
+    }
 
 }
