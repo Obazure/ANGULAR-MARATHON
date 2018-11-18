e226448d1898788c6553d48b65baf5d49cbf5b55
Obazure <conprefor@gmail.com>
2018-11-18 18:14:49 +0600

    22 lesson done

--- src/app/block4/directives/directive-first.directive.ts
+++ src/app/block4/directives/directive-first.directive.ts
@@ -1,17 +1,25 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appDirectiveFirst]'
 })
-export class DirectiveFirstDirective implements OnInit {
+export class DirectiveFirstDirective {
+
+    @HostBinding('style.backgroundColor') background: string;
 
     constructor(private element: ElementRef, private renderer: Renderer2) {
     }
 
-    ngOnInit() {
-        const {nativeElement} = this.element;
-        this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
-        this.renderer.addClass(nativeElement, 'white-text');
+    @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) {
+        // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
+        this.background = 'green';
+        this.renderer.addClass(this.element.nativeElement, 'white-text');
+    }
+
+    @HostListener('mouseleave', ['$event']) mouseLeave(event: Event) {
+        // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
+        this.background = 'transparent';
+        this.renderer.removeClass(this.element.nativeElement, 'white-text');
     }
 
 }
