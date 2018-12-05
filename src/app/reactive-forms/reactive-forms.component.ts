74a7f476175378da762be5e0a5d1c3428257214a
yekossynbay <conprefor@gmail.com>
2018-12-06 01:02:00 +0600

    41 lesson done. reactive form validation

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-forms',
@@ -19,8 +19,8 @@ export class ReactiveFormsComponent implements OnInit {
 
     ngOnInit() {
         this.form = new FormGroup({
-            email: new FormControl(''),
-            pass: new FormControl(''),
+            email: new FormControl('', [Validators.required, Validators.email]),
+            pass: new FormControl('', [Validators.required]),
             country: new FormControl('ru'),
             answer: new FormControl('no')
         });
