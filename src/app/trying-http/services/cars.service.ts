9e340ef8965f1eae0ff27acaad5944fa8bd11ae9
yekossynbay <conprefor@gmail.com>
2018-12-06 15:32:09 +0600

    50 lesson done. delete

--- src/app/trying-http/services/cars.service.ts
+++ src/app/trying-http/services/cars.service.ts
@@ -1,21 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/internal/Observable";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CarsService {
 
-  constructor(
-      private http: HttpClient,
-  ) { }
+    constructor(
+        private http: HttpClient,
+    ) {
+    }
 
-  getCars(): Observable<any> {
-    return this.http.get('http://localhost:3000/cars');
-  }
+    getCars(): Observable<any> {
+        return this.http.get('http://localhost:3000/cars');
+    }
 
-    addCar(car): Observable<any>{
-      return this.http.post('http://localhost:3000/cars', car);
+    addCar(car): Observable<any> {
+        return this.http.post('http://localhost:3000/cars', car);
+    }
+
+    changeColor(car: any) {
+        return this.http.put(`http://localhost:3000/cars/${car.id}`, car)
+    }
+
+    deleteCar(car: any){
+        return this.http.delete(`http://localhost:3000/cars/${car.id}`)
     }
 }
