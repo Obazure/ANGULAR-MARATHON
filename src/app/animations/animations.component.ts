d8b7b8c9c2d8f8059feed9f97ee198ca64881d07
yekossynbay <conprefor@gmail.com>
2018-12-28 05:10:01 +0600

    69 lesson done. complex transition

--- src/app/animations/animations.component.ts
+++ src/app/animations/animations.component.ts
@@ -26,12 +26,36 @@ import {timeInterval} from "rxjs/operators";
             transition('start <=> end', animate('1000ms ease-in')),
             transition('start => active', animate(400)),
             transition('active => end', animate(400))
+        ]),
+        trigger('multi',[
+            state('start', style({
+                backgroundColor: 'blue',
+                width: '150px',
+                height: '150px',
+                border: '5px solid black'
+            })),
+            state('end', style({
+                backgroundColor: 'red',
+                width: '170px',
+                height: '170px',
+            })),
+            transition('start <=> end',
+                [
+                    style({
+                        backgroundColor: 'yellow'
+                    }),
+                    animate(1500, style({
+                        backgroundColor: 'green'
+                    })),
+                    animate(1000)
+                ])
         ])
     ]
 })
 export class AnimationsComponent implements OnInit {
 
     state = 'start';
+    multistate = 'start';
 
     constructor() {
     }
